a718e12f8de7f4922c2fa0b12ca6ceb27ea4c56d
Rafael Maitin <145060571+Rafaelmaitin@users.noreply.github.com>
2025-02-28 14:13:07 +0100

    Create array.js
    Operacion sencilla de array para ver los commit

---
+++ array.js
@@ -0,0 +1,13 @@
+let myArray = [1];
+let myArray2 = new Array("Rafael", "Maitin", "rafa", 3, true); 
+myArray = [1];
+
+console.log(myArray2);
+
+myArray2 = new Array(3);
+myArray[2] = "Rafael";
+myArray[3] = "Maitin";
+myArray[4] = "rafa";
+myArray[5] = 3;
+myArray[6] = true;
+console.log(myArray2);
